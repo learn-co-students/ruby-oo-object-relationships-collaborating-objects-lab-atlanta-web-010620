c1a5e136ff898a1cb31a747ac15f45fcf36fce37
github username <github email address>
2020-01-09 17:56:49 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+class Artist
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def self.find_or_create_by_name(name)
+        if @@all.find {|artist| artist.name == name}
+            @@all.find {|artist| artist.name == name}
+        else
+            self.new(name)
+        end
+    end
+
+    def print_songs
+        puts self.songs.map {|song| song.name}
+    end
+end
